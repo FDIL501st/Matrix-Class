2b77bff7f4ddd5a718e1815b7299caec45549da9
FDIL <fdil.fadilh1@gmail.com>
2021-12-12 14:50:26 -0700

    Added in some throws and exception handling
    out_of_range exception thrown when trying to access outside of matrix.
    
    Some exception handling for when heap can't be allocated when trying to make the matrix.
    If bad_alloc caught, then send an error message that it was caught.
    Also make row or columns to 0 and keep matrix uniform, especially if caught when allocating each row.
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

--- Test.cpp
+++ Test.cpp
@@ -1,10 +1,9 @@
 //File to test methods of Matrix class
 //Compilation will require both Matrix.cpp and Test.cpp to be compiled
 
-#include <string>
 #include "Matrix.h" //Matrix.h has cout and endl as part of it
 using mtx::Matrix;
-using std::string;
+
 int main(int argc, char** argv)
 {
     if (argc != 3)
@@ -14,15 +13,27 @@ int main(int argc, char** argv)
         return 0;
     }
 #if 1
-    int r = argv[1][0] - '0';
-    int c = argv[2][0] - '0';
+
+#if 1
+    string str_r(argv[1]);
+    string str_c(argv[2]);
+    int r = stoi(str_r);
+    int c = stoi(str_c);
     Matrix m1(r,c);
     m1.at(0,0) = 5.46;
     m1.at(1,2) = -4.67;
+    //m1.at(-1,6) = 5;
     m1.print();
     //Test has shown that the contructor works and the print function works.
     //Also shows that the at() properly returns a reference to an entry 
     //  and can change that entry
+
+    //Tested the out_of_range exceptions being thrown as well,
+    //  they work for numbers larger than max row/column
+
+    //bad_alloc exception in constructor also catches the exception and sets row or column to 0
+    //However, because exception gets caught, program isn't terminated.
+    //Just end up with no allocation of memory, thus matrix not made as requested.
 #endif
 
 #if 0
@@ -45,6 +56,7 @@ int main(int argc, char** argv)
 #endif
 #if 1
     *(m2.at(0)+1) = 3;
+    //*(m2.at(10)+1) = 3;
     //m2.at(1) = m2.at(0);
     //Test shown that was able to get a double* returned
     //Also shows that m2.at(1) can't be modified, however the dereferenced value can.
@@ -62,5 +74,28 @@ int main(int argc, char** argv)
     //Test has shown that the = operator works fine for deepcopy of matrix objects.
     //Also that each matrix object is a seperate object
 #endif
+    //Tested the out_of_range exceptions, they work
+#endif
+
+#if 0
+
+    string str("Hello");
+    try
+    {
+        cout << str.at(10) << endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+    cout << str.at(10) << endl;
+
+    //Code showing how exception handling works
+    // and how unhandled exceptions work
+#endif
+
     return 1;
+
+
 }
